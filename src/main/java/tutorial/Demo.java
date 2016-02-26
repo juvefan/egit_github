35d1a06c85cc6e06b4f0793258c4f82959b6ce60
juvefan <evg.and.juvefan@gmail.com>
2016-02-26 21:48:43 +0400

    first commit

---
+++ src/main/java/tutorial/Demo.java
@@ -0,0 +1,9 @@
+package tutorial;
+
+public class Demo {
+
+	public boolean getBool() {
+		return true;
+	}
+
+}
---
+++ src/test/java/tutorial/DemoTest.java
@@ -0,0 +1,13 @@
+package tutorial;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class DemoTest {
+
+	@Test
+	public void shouldReturnTrue(){
+		Demo demo = new Demo();
+		assertTrue(demo.getBool());
+	}
+}
